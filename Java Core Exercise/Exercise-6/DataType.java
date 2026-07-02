881783999d1ed20485925ce8de527cea842876c0
PushkarVarshney1 <varshneypushkar01@gmail.com>
2026-07-02 12:51:23 +0530

    Add files via upload

---
+++ Java Core Exercise/Exercise-6/DataType.java
@@ -0,0 +1,20 @@
+package JavaCoreExercise;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 21;
+        float percentage = 85.5f;
+        double pi = 3.14159265359;
+        char grade = 'A';
+        boolean isPass = true;
+
+        System.out.println("Integer value: " + age);
+        System.out.println("Float value: " + percentage);
+        System.out.println("Double value: " + pi);
+        System.out.println("Character value: " + grade);
+        System.out.println("Boolean value: " + isPass);
+	}
+
+}
